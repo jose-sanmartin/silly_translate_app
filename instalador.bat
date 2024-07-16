c8426fb78b6e60ae9abda816ac4fcd1dc0d098d0
Jose San Martin <142695716+jose-sanmartin@users.noreply.github.com>
2024-07-16 13:33:16 -0400

    Add files via upload

---
+++ instalador.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Verificando Python...
+where python >nul 2>nul || (
+    echo Instalando Python...
+    start /wait "" "%~dp0python-installer.exe" /quiet InstallAllUsers=1 PrependPath=1
+)
+
+echo Clonando repositorio...
+git clone https://github.com/jose-sanmartin/silly_translate_app.git
+
+cd silly_translate_app-main
+echo Instalando requerimientos...
+pip install -r requirements.txt
+
+echo Instalación completa. Ejecutando la aplicación...
+python script.py
+pause
